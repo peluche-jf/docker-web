4505f70eb6a1fe37cb9ad39860461a2c19bcf7e0
felipesms2 <fmbh.1989@gmail.com>
2025-02-21 09:20:22 -0300

    entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+mkdir /old-wp
+chmod -R 777 /old-wp
+apt update
+apt install zsh
+useradd -m -s /usr/bin/zsh sites
+sudo usermod -aG sudo sites
+pip install requests beautifulsoup4 --break-system-packages
+chmod -R 777 /app/
+rm -rf /app/database/database.sqlite
+touch /app/database/database.sqlite && \
+chmod -R 777 /app/database/database.sqlite
+cd /app/ && composer install && \
+cp .env.example .env && \
+php artisan key:generate && \
+# php artisan migrate:fresh --seed && \
+npm install && \
+npm run build &&
+chmod -R 777 /app
+sudo a2enmod rewrite
+service apache2 start
+service ssh start
