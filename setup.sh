d99052f8ea4bb58d7710871809f622ce17bc2ea0
felipesms2 <fmbh.1989@gmail.com>
2025-02-21 11:17:19 -0300

    mkdir config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+mkdir -p resources/config
 sudo apt update && sudo apt install git -y
 FOLDER_NAME=$(tr -dc 'a-z' </dev/urandom | head -c 10)
 git clone https://github.com/felipesms2/docker-web.git "$FOLDER_NAME"
