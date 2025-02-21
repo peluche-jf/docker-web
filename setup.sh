7459f16a621672d928ea8d68a28515cfe2869bd3
felipesms2 <fmbh.1989@gmail.com>
2025-02-21 11:27:37 -0300

    env vars

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ mkdir -p resources/config
 sudo apt update && sudo apt install git -y
 FOLDER_NAME=$(tr -dc 'a-z' </dev/urandom | head -c 10)
 git clone https://github.com/felipesms2/docker-web.git "$FOLDER_NAME"
+sudo chmod -R 777 . 
 mv "$FOLDER_NAME"/* "$FOLDER_NAME"/.* . 2>/dev/null
 rm "$FOLDER_NAME" -rf
-mv ports.conf 000-default.conf envars resources/config/
+mv -f ports.conf 000-default.conf envars resources/config/
 rm setup.sh -rf
