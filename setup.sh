19ba2b82bd7a6fd4f8567be9c4e553d0bdf1587d
felipesms2 <fmbh.1989@gmail.com>
2025-02-21 10:40:05 -0300

    recursive force

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ sudo apt update && sudo apt install git -y
 FOLDER_NAME=$(tr -dc 'a-z' </dev/urandom | head -c 10)
 git clone https://github.com/felipesms2/docker-web.git "$FOLDER_NAME"
 mv "$FOLDER_NAME"/* "$FOLDER_NAME"/.* . 2>/dev/null
-rmdir "$FOLDER_NAME"
+rm "$FOLDER_NAME" -rf
+rm setup.sh -rf
